0fc67538fe69a03c25a12b498cf8e2067d076db1
Pickle Rick <dmryan711@gmail.com>
2019-04-16 17:01:47 -0400

    Added test to server.js

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES("The Big Henry", FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Alright, Alright, Alright, Alright", TRUE);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Frodo's Bag Hands", FALSE);
+
+INSERT INTO burgers(burger_name, devoured)
+
+VALUES("Baby got Fat", FALSE);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const orm = require("./config/orm");
+const burgerLoader = require('./models/burger');
+
 var app = express();
 
 
-orm.selectAll("burgers");
-//orm.insertOne("burgers","burger_name","devoured","Test Mother Fucka",0);
-orm.end();
 
+
+//burgerLoader.addABurger("Devon's Hairy Burger",false);
+
+// burgerLoader.showAllBurgers();
