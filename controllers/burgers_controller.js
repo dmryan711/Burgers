16447abceee88092fff25e527c15364cad311967
Pickle Rick <dmryan711@gmail.com>
2019-04-16 19:16:14 -0400

    Front to back working version

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const burgerLoader = require('./models/burger');
+const burgerLoader = require('../models/burger');
 
 var router = express.Router();
+
+router.get('/',function(req,res){
+    console.log("Get is working");
+    res.render('index');
+});
+
+
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const burgerLoader = require('./models/burger');
+var express = require("express");
+var router = require("./controllers/burgers_controller");
+
+var port =  8080;
+
+var expHB = require('express-handlebars');
 
 var app = express();
 
+app.engine("handlebars", expHB({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+app.use('/',router);
+
+app.listen(port,function(){
+    console.log("Listening on port "+port);
+});
 
 
 //burgerLoader.addABurger("Devon's Hairy Burger",false);
 
 // burgerLoader.showAllBurgers();
+
+//QUESTIONS
+// Where do you handle GET / POST / PUT / DELETE, in burger controller?
+//
