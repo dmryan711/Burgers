b729963dc5de47bd722d62ca7261996f5d08aba1
Pickle Rick <dmryan711@gmail.com>
2019-04-16 21:27:07 -0400

    Added client js

---
+++ public/assets/js/client.js
@@ -0,0 +1,11 @@
+// $("body").on('click',"#submit-burger",function(event){
+//     event.preventDefault();
+//     console.log("Click");
+// });
+
+
+$("#submit-burger").on('click',function(event){
+    event.preventDefault();
+    console.log("Click");
+});
+
