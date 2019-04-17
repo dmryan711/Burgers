b1eab4ca58e7f7258d283fd13354559781858152
Pickle Rick <dmryan711@gmail.com>
2019-04-17 18:44:40 -0400

    Submit adds a burger to the DB

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,5 +1,46 @@
 $("#submit-burger").on('click',function(event){
     event.preventDefault();
     console.log("Click");
+
+    //Check if text box is blank
+    if(validateForm()){ //Value is not blank
+        var newBurger = {
+            name:$("#burger-entry").val().trim()
+        }
+        
+        $.post("/add",newBurger,function(data){
+            
+
+        });
+
+        //Empty Burger Name
+        $("#burger-entry").val("");
+
+
+       
+    }else{  //Value is Blank
+
+    }
+
+    // //submit burger to db
+    // var test = {
+    //     name:"devon"
+    // }
+    // // $.get("/",function(data){});
+
+    // // //when thats done get all burgers
+
+
 });
+
+// Form validation
+function validateForm() {
+    var isValid = true;
+    $(".form-control").each(function() {
+      if ($(this).val() === "") {
+        isValid = false;
+      }
+    });
+    return isValid;
+}
 
