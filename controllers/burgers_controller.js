f2b4502a8cd143c0ada9d672cd0823368e01c689
Pickle Rick <dmryan711@gmail.com>
2019-04-17 18:17:00 -0400

    Ui Elements Mocked_2

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,10 @@ const burgerLoader = require('../models/burger');
 var router = express.Router();
 
 router.get('/',function(req,res){
-    console.log("Get is working");
     res.render('index');
+    burgerLoader.showAllBurgers();
+    
+    
 });
 
 
