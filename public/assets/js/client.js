1223b757ce7155ad4d4021b9e93233778366fbd1
Pickle Rick <dmryan711@gmail.com>
2019-04-18 00:58:47 -0400

    Burgers are displayed

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -11,6 +11,7 @@ $("#submit-burger").on('click',function(event){
         $.post("/add",newBurger).then(function(){
            $.get('/burgers',function(data){
                 console.log(data);
+                displayBurgers(data);
              });
 
         });
@@ -31,5 +32,26 @@ function validateForm() {
       }
     });
     return isValid;
+}
+
+//Display Burgers
+function displayBurgers(burgerObjectArray =[]){
+    burgerObjectArray.forEach(element =>{
+        var $burgerBox = $("<div/>");
+        $burgerBox.addClass("burger-not-eaten burgers text-center mt-2 px-2");
+        console.log(element.id + "." +" "+element.burger_name);
+        
+        var $burgerDescription = $("<p/>");
+        $burgerDescription.text(element.id + "." +" "+element.burger_name);
+        var $eatMeButton = $("<button/>");
+        $eatMeButton.addClass("btn btn-primary");
+        $eatMeButton.text("Eat Me");
+
+        $burgerBox.append($burgerDescription);
+        $burgerBox.append($eatMeButton);
+
+        $("#burger-not-eaten-container").append($burgerBox);
+
+    });
 }
 
